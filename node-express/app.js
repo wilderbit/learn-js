107e80e487049611bf8598f95b51006ad4a3ef5e
Abrar <abrar.nitk@gmail.com>
2021-07-04 11:03:37 +0530

    session route

--- node-express/app.js
+++ node-express/app.js
@@ -3,15 +3,31 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sessionData = require('./src/data/sessions.json')
+const sessionRouter = express.Router();
 
 
 const app = express();
 app.use(morgan('combined')); // tiny
 app.use(express.static(path.join(__dirname + '/public/')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
+sessionRouter.route('/')
+    .get((req, res) => {
+        res.render("session", { sessions: sessionData, });
+    });
+
+sessionRouter.route('/:sessionID')
+    .get((req, res) => {
+        res.send("Hello single Sessions")
+    });
+
+
+app.use('/sessions', sessionRouter);
 
 app.get('/', (req, res) => {
-    res.send("Hello from my app");
+    res.render("index", {"title": "Abrar Khan", "data": ['a', 'b', 'c']});
 });
 
 const PORT = process.env.PORT;

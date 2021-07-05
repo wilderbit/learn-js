c13ee9626be5eb58687dd14b4b2c5e5cc4c1d6e8
Abrar <abrar.nitk@gmail.com>
2021-07-05 12:38:42 +0530

    query find

--- node-restful/app.js
+++ node-restful/app.js
@@ -9,7 +9,13 @@ const Book = require('./models/bookModel');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    Book.find((err, books) => {
+
+    const query = {};
+    if(req.query.genre) {
+      query.genre = req.query.genre;
+    }
+
+    Book.find(query, (err, books) => {
         if(err) {
           return res.send(err);
         }

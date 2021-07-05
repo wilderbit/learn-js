739739642b39ca8313805534fd57eff945a65f13
Abrar <abrar.nitk@gmail.com>
2021-07-05 15:20:55 +0530

    integration testing

--- node-restful/app.js
+++ node-restful/app.js
@@ -3,7 +3,16 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost/bookAPI');
+
+if (process.env.ENV === 'Test') {
+    console.log('This is a test');
+    const db = mongoose.connect('mongodb://localhost/bookAPI-Test');
+} else {
+    console.log('This is for real');
+    const db = mongoose.connect('mongodb://localhost/bookAPI');
+}
+
+
 const PORT = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 const bookRouter = require('./router/bookRouter')(Book);
@@ -17,6 +26,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to my API');
 });
 
-app.listen(PORT, () => {
+app.server = app.listen(PORT, () => {
   console.log(`Running on PORT ${PORT}`);
 });
+
+module.exports = app;

8b189a81cb0b7046a65d6ea5231b9281cab0b39a
Abrar <abrar.nitk@gmail.com>
2021-07-10 13:31:45 +0530

    author quiz

---
+++ react/react-fundamental/authorquiz/src/AuthorQuiz.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+
+function AuthorQuiz() {
+  return (
+    <div className="">
+      Author Quiz
+    </div>
+  );
+}
+
+export default AuthorQuiz;
--- react/react-fundamental/authorquiz/src/index.js
+++ react/react-fundamental/authorquiz/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthorQuiz />
   </React.StrictMode>,
   document.getElementById('root')
 );

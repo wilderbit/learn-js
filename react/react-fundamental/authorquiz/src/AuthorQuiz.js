2f8f5e82edf6f0f73534a360ed14723a72cfc6c9
Abrar <abrar.nitk@gmail.com>
2021-07-11 07:11:21 +0530

    auth quiz

--- react/react-fundamental/authorquiz/src/AuthorQuiz.js
+++ react/react-fundamental/authorquiz/src/AuthorQuiz.js
@@ -1,12 +1,53 @@
 import './App.css';
 import './bootstrap.min.css';
-import Sum from './components/Sum'
 
-function AuthorQuiz() {
+function Hero() {
+  return (<div className="row">
+    <div className="jumbotron col-10 offset-1">
+      <h1>Author Quiz</h1>
+      <p>Select the book written by the author shown</p>
+    </div>
+  </div>);
+}
+
+function Book({title}) {
+    return (<div className="answer">
+        <h4>{title}</h4>
+    </div>)
+}
+
+function Turn({author, books}) {
+    return (<div className="row turn" style={{backgroundColor: "white"}}>
+        <div className="col-4 offset-1">
+            <img src={author.imageUrl} className="authorimage" alt="Author"/>
+        </div>
+        <div className="col-6">
+            {books.map((title) => <Book title={title} key={title}/>)}
+        </div>
+    </div>)
+}
+
+function Continue() {
+    return null
+}
+
+function Footer() {
+    return (
+        <div className="row">
+            <div className="col-12">
+                <p className="text-muted credit">All the text is Awesome</p>
+            </div>
+        </div>
+    );
+}
+
+function AuthorQuiz({turnData}) {
   return (
-    <div className="">
-      Author Quiz
-    <Sum a={2} b={4}></Sum>
+    <div className="container-fluid">
+        <Hero/>
+        <Turn {...turnData}/>
+        <Continue/>
+        <Footer/>
     </div>
 
   );

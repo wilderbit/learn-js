17e988551d793c6577b34c32cfcaf4162cef813b
Abrar <abrar.nitk@gmail.com>
2021-07-17 18:00:10 +0530

    Arrays

--- javaScript/Getting-Started/src/functions.js
+++ javaScript/Getting-Started/src/functions.js
@@ -47,3 +47,38 @@ console.log("Hello".charAt(0))
 // ele.addEventListener('click', function(event){
 // console.log('click')
 // })
+
+// Showing and Hiding Object
+// ele.classList.add(className)
+// ele.classList.remove(className)
+// ele.classList.contains(className)
+
+// Arrays
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+console.log("--------------------------")
+console.log("--------- Arrays ---------")
+console.log("--------------------------")
+
+// Create and Init
+let values = [];
+let new_values = [1, 2, 3];
+let create_array = Array.of(1,2,3);
+
+// Accessing the elements
+for(let i = 0; i < new_values.length; i++) {
+    console.log(new_values[i]);
+}
+
+new_values.push(4);
+new_values.pop();
+console.log(new_values);
+new_values.push(4);
+
+// slice() and splice()
+
+const newValue = new_values.slice(1, 3); // It will create a new Array
+console.log(newValue)
+newValue.splice(1, 1) // start_position and number of element to be deleted
+console.log(newValue)
+newValue.splice(1, 0, 4)
+console.log(newValue)

7cdee00c40cb6400ed7fba9dcef1c20c53aae0d2
Abrar <abrar.nitk@gmail.com>
2021-07-17 16:41:51 +0530

    DOM

--- javaScript/Getting-Started/src/functions.js
+++ javaScript/Getting-Started/src/functions.js
@@ -11,7 +11,10 @@ fn()();
 
 
 let person = {
-    name: 'Abrar'
+    name: 'Abrar',
+    showInfo: function () {
+        console.log(this.name)
+    }
 }
 
 person['age'] = 36;
@@ -19,3 +22,28 @@ person['age'] = 36;
 person.country = 'India'
 
 console.log(person.age, person.country)
+person.showInfo()
+
+// Standard built in Objects in JavaScript
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+
+let now = new Date();
+console.log(now.toDateString())
+console.log(Math.PI)
+console.log("Hello".charAt(0))
+
+
+// Document Object Model
+// https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
+
+// getElementById return HTMLElement
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement
+
+// let ele = document.getElementById("<id>")
+// ele.style.color = '#FFF'
+// ele.style.fontWeight = '100'
+
+// If element is a button
+// ele.addEventListener('click', function(event){
+// console.log('click')
+// })

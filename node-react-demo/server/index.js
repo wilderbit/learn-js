51aaaa99eeb5e3d1b39c96e6550c975726a49b55
Abrar <abrar.nitk@gmail.com>
2021-07-24 11:27:00 +0530

    serve react app using node

---
+++ node-react-demo/server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const data = require("./data.json");
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api", (req, res) => {
+    res.json({ data: "Hello from server!" })
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});

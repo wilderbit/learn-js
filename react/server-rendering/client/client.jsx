4c63e91145c1a138a1cf5d781937a741ff6f9b13
AbrarNitk <abrar.nitk@gmail.com>
2021-08-20 20:18:18 +0530

    first component

---
+++ react/server-rendering/client/client.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App = () => (
+    <div>
+        This is a React Component.
+    </div>
+);
+
+ReactDOM.render(<App/>, document.querySelector("#Container"));

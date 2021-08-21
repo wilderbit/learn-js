098872db3eb7965adc062cf0e6182ab95932cab4
AbrarNitk <abrar.nitk@gmail.com>
2021-08-21 11:53:28 +0530

    load js in html

--- react/server-rendering/server/index.js
+++ react/server-rendering/server/index.js
@@ -1,10 +1,14 @@
 // const express = require('express');
 import express from 'express';
+import { readFileSync } from 'fs';
 
 const app = new express();
 
+app.use(express.static("dist"));
+
 app.get('/', (_req, res) => {
-    res.send(`<h1>React is awesome</h1>`)
+    let index = readFileSync('public/index.html', 'utf-8');
+    res.send(index);
 });
 
 app.listen(7777, () => {
